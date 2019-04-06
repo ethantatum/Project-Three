093adbe4df423aefdd1aec1d742deaefefd2611a
Jose Chaparro <josechaparro@Joses-MacBook-Pro.local>
2019-04-06 14:48:56 -0700

    received updated code and created Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import NavMenu from './components/Nav';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavMenu />
         <header className="App-header">
         </header>
       </div>
