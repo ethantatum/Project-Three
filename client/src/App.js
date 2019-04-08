d0ea53f3242467f5137d2c10c3e31d125882eebf
Jorge G <jogoram428@gmail.com>
2019-04-08 14:38:31 -0700

    created info section on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Landing from "./pages/Landing";
-import SignUp from "./pages/SignUp";
-import Hub from "./pages/Hub";
-import NoMatch from "./pages/NoMatch";
+import Landing from "./pages/Landing/Landing";
+// import SignUp from "./pages/SignUp";
+// import Hub from "./pages/Hub";
+// import NoMatch from "./pages/NoMatch";
 import './App.css';
 
 class App extends Component {
@@ -13,10 +13,10 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path="/" component={Landing} />
-          <Route exact path="/SignUp" component={SignUp} />
-          <Route exact path="/Hub" component={Hub} />
+          {/* <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/Hub" component={Hub} /> */}
           {/* <Route exact path="/TeacherHub" component={TeacherHub} /> */}
-          <Route component={NoMatch} />
+          {/* <Route component={NoMatch} /> */}
         </Switch>
       </div>
     </Router>
--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,61 +1,55 @@
 import React, { Component } from "react";
-import DeleteBtn from "../components/DeleteBtn";
-import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
+import InfoPanel from "../../components/InfoPanel";
+// import API from "../utils/API";
 
-class Books extends Component {
+class Landing extends Component {
   state = {
-    books: [],
-    title: "",
-    author: "",
-    synopsis: ""
+
   };
 
-  componentDidMount() {
-    this.loadBooks();
-  }
+//   componentDidMount() {
+//     this.loadBooks();
+//   }
 
-  loadBooks = () => {
-    API.getBooks()
-      .then(res =>
-        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-      )
-      .catch(err => console.log(err));
-  };
+//   loadBooks = () => {
+//     API.getBooks()
+//       .then(res =>
+//         this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+//       )
+//       .catch(err => console.log(err));
+//   };
 
-  deleteBook = id => {
-    API.deleteBook(id)
-      .then(res => this.loadBooks())
-      .catch(err => console.log(err));
-  };
+//   deleteBook = id => {
+//     API.deleteBook(id)
+//       .then(res => this.loadBooks())
+//       .catch(err => console.log(err));
+//   };
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
+//   handleInputChange = event => {
+//     const { name, value } = event.target;
+//     this.setState({
+//       [name]: value
+//     });
+//   };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.title && this.state.author) {
-      API.saveBook({
-        title: this.state.title,
-        author: this.state.author,
-        synopsis: this.state.synopsis
-      })
-        .then(res => this.loadBooks())
-        .catch(err => console.log(err));
-    }
-  };
+//   handleFormSubmit = event => {
+//     event.preventDefault();
+//     if (this.state.title && this.state.author) {
+//       API.saveBook({
+//         title: this.state.title,
+//         author: this.state.author,
+//         synopsis: this.state.synopsis
+//       })
+//         .then(res => this.loadBooks())
+//         .catch(err => console.log(err));
+//     }
+//   };
 
   render() {
     return (
-      <Container fluid>
-       
-      </Container>
+      <InfoPanel />
     );
   }
 }
 
-export default Books;
+export default Landing;
