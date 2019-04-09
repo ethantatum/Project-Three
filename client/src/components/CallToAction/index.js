8d6c17e0bab48f29777ccc1869378c6cadec62f9
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-04-09 09:05:25 -0700

    updated signin buttons

--- client/src/components/CallToAction/index.js
+++ client/src/components/CallToAction/index.js
@@ -3,13 +3,13 @@ import './style.css';
 
 function SignUpBtns () {
     return (
-        <div className="main-buttons">
-            <button variant="primary" size="lg">
-                Log In
-            </button>
-            <button variant="secondary" size="lg">
-                Sign-Up
-            </button>
+        <div className="ui two buttons">
+            <div className="ui basic green button">
+                Login
+            </div>
+            <div className="ui basic blue button">
+                Sign Up
+            </div>
         </div>
 
     )
