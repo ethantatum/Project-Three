0b33b027b1bfb9e6645bba002e88a6672f27c6a9
ltenore <leightenore@gmail.com>
2019-04-08 21:42:05 -0700

    started carousel

--- client/src/components/LandingImage/index.js
+++ client/src/components/LandingImage/index.js
@@ -3,10 +3,20 @@ import "./style.css";
 
 function LandingImage() {
   return (
-    <div>
-        <img className="landingimage" src={require('./images/usersthink.jpg')} />
+    <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src={require ("./images/1.jpg")} class="d-block" />
+        </div>
+        <div class="carousel-item">
+          <img src={require ("./images/2.jpg")} class="d-block" />
+        </div>
+        <div class="carousel-item">
+          <img src={require ("./images/4.jpg")} class="d-block" />
+        </div>
+      </div>
     </div>
   );
 }
-
+        
 export default LandingImage;
