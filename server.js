ead48dfabed759b8a4eb58e80e4329401b6c64e1
Jorge G <jogoram428@gmail.com>
2019-04-10 17:43:47 -0700

    started adding passport

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ if (process.env.NODE_ENV === "production") {
 // app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/careconnectdb");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/careconnectdb", 
+{ useNewUrlParser: true })
+.then(() => console.log("MongoDB successfully connected"))
+.catch(err => console.log(err));
 
 // Start the API server
 app.listen(PORT, function() {
