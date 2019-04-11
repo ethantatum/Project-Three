a11b8469ff7b88428054af1bead2a9278ee1bd7f
Jorge G <jogoram428@gmail.com>
2019-04-10 19:58:45 -0700

    ignore lock

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!.`);
 });
