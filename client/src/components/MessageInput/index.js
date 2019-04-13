2887b015b2a06982768fb408f6159246a7b1e208
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-04-13 13:26:47 -0700

    updated images and title hover message

--- client/src/components/MessageInput/index.js
+++ client/src/components/MessageInput/index.js
@@ -27,11 +27,11 @@ class MessageInput extends Component {
     }
 
     handlePositive() {
-        return (this.state.positiveMessage ? this.setState({positiveMessage: false}) : this.setState({positiveMessage: true}));
+        return (this.state.positiveMessage ? this.setState({positiveMessage: false}) : this.setState({positiveMessage: true, negativeMessage: false}));
     }
 
     handleNegative() {
-        return (this.state.negativeMessage ? this.setState({negativeMessage: false}) : this.setState({negativeMessage: true}));
+        return (this.state.negativeMessage ? this.setState({negativeMessage: false}) : this.setState({negativeMessage: true, positiveMessage: false}));
     }
 
     // Helper function that prevents page from loading - WILL ADD MORE FUNCTIONALITY
@@ -46,15 +46,15 @@ class MessageInput extends Component {
         let imageNeg;
 
         if(this.state.positiveMessage) {
-            imagePos = require('./images/positive-color.png');
+            imagePos = require('./images/positive-color2.png');
         } else {
-            imagePos = require('./images/positive-grey.png');
+            imagePos = require('./images/positive-grey2.png');
         }
 
         if(this.state.negativeMessage) {
-            imageNeg = require('./images/negative-color.png');
+            imageNeg = require('./images/negative-color2.png');
         } else {
-            imageNeg = require('./images/negative-grey.png');
+            imageNeg = require('./images/negative-grey2.png');
         }
 
         return (
@@ -66,28 +66,31 @@ class MessageInput extends Component {
                             placeholder="Message Title"
                             value={this.state.messageTitle}
                             onChange={this.handleChange}
-                            name="messagetitle"
+                            name="messageTitle"
                         ></input>
                     </div>
                     <div className="ui inverted divider"></div>
                     <div className="ui huge fluid inverted input">
-                        <input 
-                            type="text"
-                            placeholder="Message Body"
-                            value={this.state.messageBody}
-                            onChange={this.handleChange}
-                            name="messagebody"
-                        ></input>
+                        <div className="field">
+                            <textarea 
+                                className="fluid"
+                                type="text"
+                                placeholder="Message Body"
+                                value={this.state.messageBody}
+                                onChange={this.handleChange}
+                                name="messageBody"
+                            ></textarea>
+                        </div>
                     </div>
                     <div className="ui inverted divider"></div>
-                    <label>
-                        <input type="radio" name="positivemessage" value={positiveMessage} onClick={this.handlePositive} />
-                        <img src={imagePos} alt="smiling emoji" />
-                    </label>
-                    <label>
-                        <input type="radio" name="negativemessage" value={negativeMessage}  onClick={this.handleNegative} />
-                        <img src={imageNeg} alt="sad emoji" />
-                    </label>
+                        <label>
+                            <input type="radio" name="positivemessage" value={positiveMessage} onClick={this.handlePositive} />
+                            <img src={imagePos} alt="smiling emoji" title="Victory!" />
+                        </label>
+                        <label>
+                            <input type="radio" name="negativemessage" value={negativeMessage}  onClick={this.handleNegative} />
+                            <img src={imageNeg} alt="sad emoji" title="Challenge..." />
+                        </label>
                     <div className="ui inverted divider"></div>
                     <button className="ui inverted green button" type="submit" disabled={!this.validateForm()}>Send</button>
                 </form>
--- client/src/pages/Hub/Hub.js
+++ client/src/pages/Hub/Hub.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import faker from "faker";
+import Logo from "../../components/Logo";
 import MessageInput from "../../components/MessageInput";
 import MessageDisplay from "../../components/MessageDisplay";
 
@@ -12,6 +13,7 @@ class Hub extends Component {
         return (
             <div className="row bg-dark">
                 <div className="col-md-3">
+                    <Logo />
                     <img className="ml-3 mt-4 img-thumbnail" src={faker.image.avatar()} alt="avatar" /><br />
                     <h3 className="text-white ml-3">{faker.name.firstName()}</h3>
                     <h5 className="text-white ml-3">Messages ==></h5>
