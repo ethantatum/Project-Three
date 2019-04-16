eb849a74a46a7415b04b4512f6da15891fe09bcd
Jorge G <jogoram428@gmail.com>
2019-04-15 20:13:17 -0700

    added isTeacher to global state

--- routes/api/user.js
+++ routes/api/user.js
@@ -69,7 +69,8 @@ router.post("/login", (req, res) => {
                 // Create JWT Payload
                 const payload = {
                     id: user.id,
-                    name: user.name
+                    name: user.name,
+                    isTeacher: user.isTeacher
                 };
                 // Sign token
                 jwt.sign(
