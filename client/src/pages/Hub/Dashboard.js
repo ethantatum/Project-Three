2581ba55143075b92be8b3c0b1f02e0f95d6f3a8
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-04-15 19:06:21 -0700

    added Hub components to Dashboard component

--- client/src/pages/Hub/Dashboard.js
+++ client/src/pages/Hub/Dashboard.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import Logo from "../../components/Logo";
+import MessageInput from "../../components/MessageInput";
+import MessageDisplay from "../../components/MessageDisplay";
 
 
 
@@ -10,33 +13,45 @@ class Dashboard extends Component {
     e.preventDefault();
     this.props.logoutUser();
   };
-  
-render() {
+
+  render() {
     const { user } = this.props.auth;
-return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
+    return (
+      <div className="row bg-dark">
+        <div className="col-md-3">
+          <div style={{ height: "75vh" }} className="container valign-wrapper">
+            <div className="row">
+              <div className="col s12 center-align">
+                <h4>
+                  <b>Hey there,</b> {user.name.split(" ")[0]}
+                  <p className="flow-text grey-text text-darken-1">
+                    You are logged into a full-stack{" "}
+                    <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                  </p>
+                </h4>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem"
+                  }}
+                  onClick={this.onLogoutClick}
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Logout
+                </button>
+              </div>
+            </div>
           </div>
+          <Logo />
+          <img className="ml-3 mt-4 img-thumbnail" src={faker.image.avatar()} alt="avatar" /><br />
+          <h3 className="text-white ml-3">{faker.name.firstName()}</h3>
+          <h5 className="text-white ml-3">Messages ==></h5>
+        </div>
+        <div className="col-md-9">
+          <MessageInput />
+          <MessageDisplay />
         </div>
       </div>
     );
