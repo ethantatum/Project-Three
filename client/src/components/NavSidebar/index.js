f7f41b8b571e86a6c582b908746b48fe35f472d5
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-04-17 18:17:28 -0700

    changed span text, changed sidebar links to buttons

--- client/src/components/NavSidebar/index.js
+++ client/src/components/NavSidebar/index.js
@@ -5,30 +5,24 @@ import faker from 'faker';
 const NavSideBar = props => {
     let barClasses = 'side-bar';
     if (props.show) {
-            barClasses = 'side-bar open';
+        barClasses = 'side-bar open';
     }
 
     return (
-    <nav className={barClasses}>
-        <ul>
-            <li>
-                <img className="ml-1 mt-1 img-thumbnail" src={faker.image.avatar()} alt="avatar"/><br />
-                <h4>{props.user}</h4>
-            </li>
-            <li>
-                <a href="/">Students</a>
-            </li>
-            <li>
-                <a href="/">Messages</a>
-            </li>
-            <li>
-                <a href="/">Notes</a>
-            </li>
-            <li>
-                <a href="/">misc</a>
-            </li>
-        </ul>
-    </nav>
+        <nav className={barClasses}>
+            <ul>
+                <li>
+                    <img className="ml-1 mt-1 img-thumbnail" src={faker.image.avatar()} alt="avatar" /><br />
+                    <h4>{props.user}</h4>
+                </li>
+            </ul>
+            <div className="ui vertical buttons nav-buttons ml-5">
+                <button className="ui button">Students</button>
+                <button className="ui button">Messages</button>
+                <button className="ui button">Notes</button>
+                <button className="ui button">Profile</button>
+            </div>
+        </nav>
     );
 };
 
