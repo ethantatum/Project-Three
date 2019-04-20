2b5076f847dc075f07ddff8bfc37f5b53e8f3ef8
JoGom <jogoram428@gmail.com>
2019-04-20 09:07:10 -0700

    save

--- models/student.js
+++ models/student.js
@@ -5,15 +5,6 @@ const studentSchema = new Schema({
     name: { type: String, required: true },
     age: { type: Number, required: true },
     image: { type: String, default: "/assets/images/no-image.png"},
-    class: { type: String, required: true },
-    parents: [{
-        parent1: {
-            type: String
-        },
-        parent2: {
-            type: String
-        }
-    }],
     behaviors: [{
         type: String
     }],
@@ -22,8 +13,17 @@ const studentSchema = new Schema({
         ref: 'Comment'
     }] 
 
-});
+}, {strict: false});
 
 const Student = mongoose.model("Student", studentSchema);
 
 module.exports = Student;
+
+// parents: [{
+//     parent1: {
+//         type: String
+//     },
+//     parent2: {
+//         type: String
+//     }
+// }]
