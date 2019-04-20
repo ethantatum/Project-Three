78c30e7a97cdad997dc88c3315c96d4b091b27d7
Jorge G <jogoram428@gmail.com>
2019-04-20 14:19:16 -0700

    removed false imports

--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
 module.exports = {
     Comment: require("./Comment"),
     Message: require("./Message"),
-    Parent: require("./Parent"),
     Student: require("./Student"),
-    Teacher: require("./Teacher"),
     User: require("./User")
 };
