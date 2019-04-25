ee039f4b74a22ea85cd8726d5bb6b3295174a1bc
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-04-25 13:44:37 -0700

    working on messagecomponent

--- client/src/components/DashboardComponents/MessagesComponent/index.js
+++ client/src/components/DashboardComponents/MessagesComponent/index.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import API from "../../../utils/API";
+// import {bindActionCreators} from "redux";
+import { connect } from "react-redux";
 import MessageInput from "./MessageInput";
 import MessageDisplay from "./MessageDisplay";
+import API from '../../../utils/API';
 
 
 
-function MessageComponent(props) {
-    return (
-    <div>
-        <MessageInput />
-        <MessageDisplay />
-    </div>
-    );
+class MessageComponent extends Component {
+    state ={
+        messageTitle: "",
+        messageBody: "",
+        positiveMessage: false,
+        negativeMessage: false,
+        messageTo: "",
+        messageFrom: this.props.user.id,
+        messageArray: []
+    }
+
+    // componentDidMount() {
+    //     this.loadMessages();
+    // }
+
+    // loadMessages = () => {
+    //     API.getMessages()
+    //     .then(res => this.setState({ messageArray: res.data}))
+    //     .catch(err => console.log(err));
+    // }
+
+    // Helper function checks if there is any content in title/body input fields
+    validateForm = () => this.state.messageTitle.length > 0 && this.state.messageBody.length > 0;
+
+    render() {
+        return (
+            <div>
+                <MessageInput />
+                <MessageDisplay />
+            </div>
+        );
+    }
 }
 
-export default MessageComponent;
+//adds redux state to this component's props
+function mapStateToProps(state) {
+    return {
+        user: state.auth.user
+    }
+}
+
+//connects this component to redux
+export default connect(mapStateToProps)(MessageComponent);
