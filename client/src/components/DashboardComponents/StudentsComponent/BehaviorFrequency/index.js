77770205f692d03b23b49a9e1dbf8635fc0aa782
JoGom <jogoram428@gmail.com>
2019-04-29 12:58:35 -0700

    added functionality to add behaviors and show them

--- client/src/components/DashboardComponents/StudentsComponent/BehaviorFrequency/index.js
+++ client/src/components/DashboardComponents/StudentsComponent/BehaviorFrequency/index.js
@@ -9,24 +9,55 @@ import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 
 class BehaviorFrequency extends React.Component {
-
-    state = {
-        behavior: [],
-        frequency: 0,
-        type: ""
-    };
+    constructor(props) {
+        super(props)
+        this.state = {
+            behavior: [],
+            frequency: 0,
+            studentID: props.match.params.studentID,
+            studentName: "",
+            behaviorName: "",
+            type: ""
+        };
+    }
 
     componentDidMount = () => {
         this.loadBehaviors();
-        };
+    };
+
+    //for form input
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    };
+
+    //function that runs when user clicks the forms submit button
+    handleSubmit = event => {
+        event.preventDefault();
+        
+        if (this.state.behaviorName && this.state.type) {
+        API.addBehavior(this.state.studentID, {
+            behavior: this.state.behaviorName,
+            type: this.state.type
+        })
+        .then((res) => {
+            console.log(res);
+            this.loadBehaviors();
+        })
+        .catch(err => console.log(err));  
+        console.log("formsubmit");
+        }
+    };
+    
     
     //Loads the Behaviors that belong to the current student
     loadBehaviors = () => {
-    API.getBehavior()
+    API.getBehavior(this.state.studentID)
         .then(res =>
         {
             console.log(res);
-            this.setState({ behavior: res.data });
+            this.setState({ behavior: res.data.behaviors, studentName: `${res.data.firstname} ${res.data.lastname}`});
         })
         .catch(err => console.log(err));
     };
@@ -41,19 +72,30 @@ class BehaviorFrequency extends React.Component {
 
     render() {
         return (
-            <CardComponent headerText = {`Behaviors - `}>
+            <CardComponent headerText = {`Behaviors - ${this.state.studentName}`}>
                 <div className="container-fluid p-2">
                     <h1>Behavior Frequency Counters</h1>
 
-                    <AddBehavior />
+                    <AddBehavior 
+                        typeState={this.state.type}
+                        behaviorState={this.state.behaviorName}
+                        handleChange={this.handleChange}
+                        handleSubmit={this.handleSubmit}
+                    />
 
                     <hr></hr>
 
                     <CounterButtons
-                        frequency={this.state.frequency}
-                        behavior={this.state.behavior}
-                        handleIncrement={this.handleIncrement}
-                        handleDecrement={this.handleDecrement} />
+                    // key={behavior._id}
+                    // id={behavior._id}
+                    // frequency={behavior.frequency}
+                    // behavior={behavior.behavior}
+                    behaviors={this.state.behavior}
+                    handleIncrement={this.handleIncrement}
+                    handleDecrement={this.handleDecrement}
+                    />
+
+
                 </div>
             </CardComponent>
         );
--- client/src/components/DashboardComponents/StudentsComponent/CounterButtons/CounterButtons.js
+++ client/src/components/DashboardComponents/StudentsComponent/CounterButtons/CounterButtons.js
@@ -1,80 +1,81 @@
 import React from "react";
 import './CounterButtons.css'
 
-const behaviors = [
-    {
-        id: 1,
-        behavior: "Hitting",
-        type: "negative"
-    },
-    {
-        id: 2,
-        behavior: "Kicking",
-        type: "negative"
-    },
-    {
-        id: 3,
-        behavior: "Scratching",
-        type: "negative"
-    },
-    {
-        id: 4,
-        behavior: "Biting",
-        type: "negative"
-    },
-    {
-        id: 5,
-        behavior: "Spitting",
-        type: "negative"
-    },
-    {
-        id: 6,
-        behavior: "Elopement",
-        type: "negative"
-    },
-    {
-        id: 7,
-        behavior: "Yelling",
-        type: "negative"
-    },
-    {
-        id: 8,
-        behavior: "Self Injury",
-        type: "negative"
-    },
-    {
-        id: 9,
-        behavior: "Property Destruction",
-        type: "negative"
-    },
-    {
-        id: 10,
-        behavior: "Verbal Aggression",
-        type: "negative"
-    },
-    {
-        id: 11,
-        behavior: "Making a Request",
-        type: "positive"
-    },
-    {
-        id: 12,
-        behavior: "Responding to Demand",
-        type: "positive"
-    },
-    {
-        id: 13,
-        behavior: "Answering a Question",
-        type: "positive"
-    },
-    {
-        id: 14,
-        behavior: "Talking to Peer",
-        type: "positive"
-    }
-]
+// const behaviors = [
+//     {
+//         id: 1,
+//         behavior: "Hitting",
+//         type: "negative"
+//     },
+//     {
+//         id: 2,
+//         behavior: "Kicking",
+//         type: "negative"
+//     },
+//     {
+//         id: 3,
+//         behavior: "Scratching",
+//         type: "negative"
+//     },
+//     {
+//         id: 4,
+//         behavior: "Biting",
+//         type: "negative"
+//     },
+//     {
+//         id: 5,
+//         behavior: "Spitting",
+//         type: "negative"
+//     },
+//     {
+//         id: 6,
+//         behavior: "Elopement",
+//         type: "negative"
+//     },
+//     {
+//         id: 7,
+//         behavior: "Yelling",
+//         type: "negative"
+//     },
+//     {
+//         id: 8,
+//         behavior: "Self Injury",
+//         type: "negative"
+//     },
+//     {
+//         id: 9,
+//         behavior: "Property Destruction",
+//         type: "negative"
+//     },
+//     {
+//         id: 10,
+//         behavior: "Verbal Aggression",
+//         type: "negative"
+//     },
+//     {
+//         id: 11,
+//         behavior: "Making a Request",
+//         type: "positive"
+//     },
+//     {
+//         id: 12,
+//         behavior: "Responding to Demand",
+//         type: "positive"
+//     },
+//     {
+//         id: 13,
+//         behavior: "Answering a Question",
+//         type: "positive"
+//     },
+//     {
+//         id: 14,
+//         behavior: "Talking to Peer",
+//         type: "positive"
+//     }
+// ]
 
 function CounterButtons(props) {
+    
     return (
         <div>
             <div className="row">
@@ -82,10 +83,10 @@ function CounterButtons(props) {
                     <div className="row behaviorTypeHeader">
                     <h3>Negative Behaviors</h3>
                     </div>
-                    {behaviors.filter(behavior => props.type === "negative").map(button => (
-                        <div key={button.id} className="btn-group btn-group-lg" role="group" aria-label="Basic example">
+                    {props.behaviors.filter(behavior => behavior.type === "negative").map(button => (
+                        <div key={button._id} className="btn-group btn-group-lg" role="group" aria-label="Basic example">
                             <button type="button" className="btn btn-danger" onClick={props.handleDecrement}>-</button>
-                            <span id="btnSpan" className="bg-danger">{props.behavior}<br />{props.frequency}</span>
+                            <span id="btnSpan" className="bg-danger">{button.behavior}<br />{button.frequency}</span>
                             <button type="button" className="btn btn-danger" onClick={props.handleIncrement}>+</button>
                         </div>
                     ))}
@@ -96,10 +97,10 @@ function CounterButtons(props) {
                     <h3>Positive Behaviors</h3>
                     </div>
 
-                    {behaviors.filter(behavior => props.type === "positive").map(button => (
-                        <div key={button.id} className="btn-group btn-group-lg" role="group" aria-label="Basic example">
+                    {props.behaviors.filter(behavior => behavior.type === "positive").map(button => (
+                        <div key={button._id} className="btn-group btn-group-lg" role="group" aria-label="Basic example">
                             <button type="button" className="btn btn-success" onClick={props.handleDecrement}>-</button>
-                            <span id="btnSpan" className="bg-success">{props.behavior}<br />{props.frequency}</span>
+                            <span id="btnSpan" className="bg-success">{button.behavior}<br />{button.frequency}</span>
                             <button type="button" className="btn btn-success" onClick={props.handleIncrement}>+</button>
                         </div>
                     ))
