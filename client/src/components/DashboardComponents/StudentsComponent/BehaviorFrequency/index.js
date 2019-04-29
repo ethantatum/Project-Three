151a444d138b031e145a8dfa03fc01f244ce3c8e
JoGom <jogoram428@gmail.com>
2019-04-29 15:43:00 -0700

    added functions to increment or decrement behaviors

--- client/src/components/DashboardComponents/StudentsComponent/BehaviorFrequency/index.js
+++ client/src/components/DashboardComponents/StudentsComponent/BehaviorFrequency/index.js
@@ -13,7 +13,6 @@ class BehaviorFrequency extends React.Component {
         super(props)
         this.state = {
             behavior: [],
-            frequency: 0,
             studentID: props.match.params.studentID,
             studentName: "",
             behaviorName: "",
@@ -62,20 +61,43 @@ class BehaviorFrequency extends React.Component {
         .catch(err => console.log(err));
     };
 
-    handleIncrement = () => {
-        this.setState({ frequency: this.state.frequency + 1 });
+    handleIncrement = (behaviorID) => {
+        const newbehaviorArr = this.state.behavior.map(behavior => {
+            let freq = (behavior._id === behaviorID) ? behavior.frequency + 1 : behavior.frequency;
+            return {...behavior, frequency: freq};
+        });
+        this.setState({behavior: newbehaviorArr});
+        console.log(newbehaviorArr);
+        
     };
 
-    handleDecrement = () => {
-        this.setState({ frequency: this.state.frequency - 1 });
+    handleDecrement = (behaviorID) => {
+        const newbehaviorArr = this.state.behavior.map(behavior => {
+            let freq = (behavior._id === behaviorID) ? behavior.frequency - 1 : behavior.frequency;
+            return {...behavior, frequency: freq};
+        });
+        this.setState({behavior: newbehaviorArr});
+        console.log(newbehaviorArr);
     };
 
+    startObservation = () => {
+        const newbehaviorArr = this.state.behavior.map(behavior => {
+            return {...behavior, frequency: 0};
+        });
+        this.setState({behavior: newbehaviorArr});
+        console.log(newbehaviorArr);
+    }
+
+    endObservation = () => {
+
+    }
     render() {
         return (
             <CardComponent headerText = {`Behaviors - ${this.state.studentName}`}>
                 <div className="container-fluid p-2">
                     <h1>Behavior Frequency Counters</h1>
 
+                    <button onClick={this.startObservation}>Start Observation</button>
                     <AddBehavior 
                         typeState={this.state.type}
                         behaviorState={this.state.behaviorName}
--- client/src/components/DashboardComponents/StudentsComponent/CounterButtons/CounterButtons.js
+++ client/src/components/DashboardComponents/StudentsComponent/CounterButtons/CounterButtons.js
@@ -1,78 +1,6 @@
 import React from "react";
 import './CounterButtons.css'
 
-// const behaviors = [
-//     {
-//         id: 1,
-//         behavior: "Hitting",
-//         type: "negative"
-//     },
-//     {
-//         id: 2,
-//         behavior: "Kicking",
-//         type: "negative"
-//     },
-//     {
-//         id: 3,
-//         behavior: "Scratching",
-//         type: "negative"
-//     },
-//     {
-//         id: 4,
-//         behavior: "Biting",
-//         type: "negative"
-//     },
-//     {
-//         id: 5,
-//         behavior: "Spitting",
-//         type: "negative"
-//     },
-//     {
-//         id: 6,
-//         behavior: "Elopement",
-//         type: "negative"
-//     },
-//     {
-//         id: 7,
-//         behavior: "Yelling",
-//         type: "negative"
-//     },
-//     {
-//         id: 8,
-//         behavior: "Self Injury",
-//         type: "negative"
-//     },
-//     {
-//         id: 9,
-//         behavior: "Property Destruction",
-//         type: "negative"
-//     },
-//     {
-//         id: 10,
-//         behavior: "Verbal Aggression",
-//         type: "negative"
-//     },
-//     {
-//         id: 11,
-//         behavior: "Making a Request",
-//         type: "positive"
-//     },
-//     {
-//         id: 12,
-//         behavior: "Responding to Demand",
-//         type: "positive"
-//     },
-//     {
-//         id: 13,
-//         behavior: "Answering a Question",
-//         type: "positive"
-//     },
-//     {
-//         id: 14,
-//         behavior: "Talking to Peer",
-//         type: "positive"
-//     }
-// ]
 
 function CounterButtons(props) {
     
@@ -85,9 +13,9 @@ function CounterButtons(props) {
                     </div>
                     {props.behaviors.filter(behavior => behavior.type === "negative").map(button => (
                         <div key={button._id} className="btn-group btn-group-lg" role="group" aria-label="Basic example">
-                            <button type="button" className="btn btn-danger" onClick={props.handleDecrement}>-</button>
+                            <button type="button" className="btn btn-danger" onClick={() => props.handleDecrement(button._id)}>-</button>
                             <span id="btnSpan" className="bg-danger">{button.behavior}<br />{button.frequency}</span>
-                            <button type="button" className="btn btn-danger" onClick={props.handleIncrement}>+</button>
+                            <button type="button" className="btn btn-danger" onClick={() => props.handleIncrement(button._id)}>+</button>
                         </div>
                     ))}
                 </div>
@@ -99,9 +27,9 @@ function CounterButtons(props) {
 
                     {props.behaviors.filter(behavior => behavior.type === "positive").map(button => (
                         <div key={button._id} className="btn-group btn-group-lg" role="group" aria-label="Basic example">
-                            <button type="button" className="btn btn-success" onClick={props.handleDecrement}>-</button>
+                            <button type="button" className="btn btn-success" onClick={() => props.handleDecrement(button._id)}>-</button>
                             <span id="btnSpan" className="bg-success">{button.behavior}<br />{button.frequency}</span>
-                            <button type="button" className="btn btn-success" onClick={props.handleIncrement}>+</button>
+                            <button type="button" className="btn btn-success" onClick={() => props.handleIncrement(button._id)}>+</button>
                         </div>
                     ))
                     }
