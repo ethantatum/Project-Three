55066b6a568db0bf6d318e7f65eee26706afcd32
animus apertus <37455165+ethantatum@users.noreply.github.com>
2019-04-29 18:49:07 -0700

    testing styling

--- client/src/components/DashboardComponents/CardComponent/index.js
+++ client/src/components/DashboardComponents/CardComponent/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 
 const CardComponent = (props) => {
     return (
-        <div className="card w-75" id="cardCont">
+        <div className="card ml-5 mt-3 w-75" id="cardCont">
             <h5 className="card-header">{props.headerText}</h5>
             <div className="card-body cardBody">
                 {props.children}
